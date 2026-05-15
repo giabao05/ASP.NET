12b04488e666397677e4a4fb601897c7915a8179
ASP.NET <giabaolesag@gmail.com>
2026-05-15 14:28:43 +0700

    Khoi tao du an BaoCMS

---
+++ CMS.Data/Entitis/Category.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Sinh viên: Lê Gia Bảo
+ * Mã sinh viên: 2123110193
+ * Lớp: CCQ2311F
+ * Ngày tạo: 15/5/2026
+ * Mô tả: Thực hiện quản lí danh mục
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entitis
+{
+   public class Category
+    {
+            public int Id { get; set; }
+        public string Name { get; set; }//Tên danh mục
+        public string Description { get; set; } 
+        
+        //Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+
+
+    }
+}
---
+++ CMS.Data/Entitis/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entitis
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }//Tiêu đề bài viết
+        public string Content { get; set; }//Nội dung chi tiết
+        public string ImagaUrl { get ; set; }//Hình ảnh đại diện
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+        //Khóa ngoại liên kết với Category
+        public int CategoryId {  get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
